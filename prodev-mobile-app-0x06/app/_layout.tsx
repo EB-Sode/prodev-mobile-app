e3a8d9dc0eb622cd7d4245d752798fe271d1b9fb
Blessed Sode <emi.square23@gmail.com>
2026-01-13 20:00:52 +0100

    profile override

--- prodev-mobile-app-0x06/app/_layout.tsx
+++ prodev-mobile-app-0x06/app/_layout.tsx
@@ -1,14 +1,24 @@
 import { Stack } from "expo-router";
-import { View } from 'react-native'
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{
-      headerShown: false,
-    }}>
-      <Stack.Screen name="/" />
+    <Stack
+      screenOptions={{
+        headerShown: false, // default for all screens
+      }}
+    >
+      <Stack.Screen name="index" />
       <Stack.Screen name="join" />
       <Stack.Screen name="signin" />
+
+      {/* Profile overrides the default */}
+      <Stack.Screen
+        name="profile"
+        options={{
+          headerShown: true,
+          title: "Profile",
+        }}
+      />
     </Stack>
   );
 }
